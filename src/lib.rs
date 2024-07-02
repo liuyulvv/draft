bf2a313a19af22a19ca5eb84b12cf85077575e9e
liuyulvv <liuyulvv@outlook.com>
2024-07-02 13:48:43 +0800

    style: ignore result

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ pub async fn run() {
     cfg_if::cfg_if! {
         if #[cfg(target_arch = "wasm32")] {
             std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-            console_log::init_with_level(log::Level::Debug);
+            let _ = console_log::init_with_level(log::Level::Debug);
         } else {
             env_logger::init();
         }
