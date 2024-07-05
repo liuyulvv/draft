ffafe13c269b753a2d749f45289f2cd084035532
liuyulvv <liuyulvv@outlook.com>
2024-07-05 14:04:29 +0800

    refactor: bevy

--- src/lib.rs
+++ src/lib.rs
@@ -1,51 +1,46 @@
-mod app;
-mod state;
-mod util;
+mod event;
 
-use crate::app::App;
+use bevy::prelude::*;
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
-use winit::event_loop::{ControlFlow, EventLoop};
+
 #[cfg(target_arch = "wasm32")]
-use winit::platform::web::EventLoopExtWebSys;
+#[wasm_bindgen(start)]
+pub fn run() {
+    App::new()
+        .insert_resource(ClearColor(Color::srgb(0.5, 0.5, 0.9)))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Draft".into(),
+                name: Some("Draft".into()),
+                canvas: Some("#main_canvas".into()),
+                fit_canvas_to_parent: true,
+                ..Default::default()
+            }),
+            ..default()
+        }))
+        .add_systems(Startup, setup)
+        .run();
+}
 
 #[cfg(not(target_arch = "wasm32"))]
 pub fn run() {
-    let file_appender = tracing_appender::rolling::hourly("./", "draft.log");
-    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-    tracing_subscriber::fmt()
-        .with_writer(non_blocking)
-        .with_ansi(false)
-        .with_file(true)
-        .with_line_number(true)
-        .init();
-
-    let event_loop = EventLoop::new();
-    match event_loop {
-        Ok(event_loop) => {
-            event_loop.set_control_flow(ControlFlow::Poll);
-            #[allow(unused_mut)]
-            let mut app = App::new();
-            let _ = event_loop.run_app(&mut app);
-        }
-        Err(e) => log::error!("Error creating event loop: {:?}", e),
-    }
+    App::new()
+        .insert_resource(ClearColor(Color::srgb(0.5, 0.5, 0.9)))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Draft".into(),
+                name: Some("Draft".into()),
+                canvas: Some("#main_canvas".into()),
+                fit_canvas_to_parent: true,
+                ..Default::default()
+            }),
+            ..default()
+        }))
+        .add_systems(Startup, setup)
+        .run();
 }
 
-#[cfg(target_arch = "wasm32")]
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
-pub async fn run() {
-    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-    let _ = console_log::init_with_level(log::Level::Debug);
-
-    let event_loop = EventLoop::new();
-    match event_loop {
-        Ok(event_loop) => {
-            event_loop.set_control_flow(ControlFlow::Poll);
-            #[allow(unused_mut)]
-            let mut app = App::new().await;
-            event_loop.spawn_app(app);
-        }
-        Err(e) => log::error!("Error creating event loop: {:?}", e),
-    }
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
