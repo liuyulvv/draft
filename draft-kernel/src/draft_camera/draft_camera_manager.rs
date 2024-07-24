977582f71b830c4e4053e1d11816e4d704ac6e3a
liuyulvv <liuyulvv@outlook.com>
2024-07-24 17:53:52 +0800

    update: switch camera on desktop

--- draft-kernel/src/draft_camera/draft_camera_manager.rs
+++ draft-kernel/src/draft_camera/draft_camera_manager.rs
@@ -1,3 +1,9 @@
+#[cfg(not(target_arch = "wasm32"))]
+use winit::{
+    event::ElementState,
+    keyboard::{Key, KeyCode, PhysicalKey},
+};
+
 use super::{
     draft_camera::DraftCamera, draft_camera_orthographic::DraftCameraOrthographic,
     draft_camera_perspective::DraftCameraPerspective, draft_camera_uniform::DraftCameraUniform,
@@ -13,7 +19,7 @@ impl DraftCameraManager {
         Self {
             camera: vec![
                 Box::new(DraftCameraOrthographic::new(
-                    glam::Vec3::new(0.0, 5.0, 10.0),
+                    glam::Vec3::new(0.0, 0.0, 10.0),
                     glam::Vec3::new(0.0, 0.0, 0.0),
                 )),
                 Box::new(DraftCameraPerspective::new(
@@ -25,7 +31,7 @@ impl DraftCameraManager {
         }
     }
 
-    pub fn _set_active_camera(&mut self, index: usize) {
+    pub fn set_active_camera(&mut self, index: usize) {
         if index < self.camera.len() {
             self.active_camera = index;
         }
@@ -45,5 +51,25 @@ impl DraftCameraManager {
         let camera = self.active_camera();
         let camera_matrix = camera.projection_matrix() * camera.view_matrix();
         camera_uniform.update_view_proj(camera.position(), camera_matrix);
+    }
+
+    pub fn process_keyboard(
+        &mut self,
+        physical_key: &PhysicalKey,
+        logical_key: &Key,
+        state: &ElementState,
+    ) -> bool {
+        #[cfg(not(target_arch = "wasm32"))]
+        match physical_key {
+            PhysicalKey::Code(KeyCode::Numpad2) | PhysicalKey::Code(KeyCode::Digit2) => {
+                self.set_active_camera(0)
+            }
+            PhysicalKey::Code(KeyCode::Numpad3) | PhysicalKey::Code(KeyCode::Digit3) => {
+                self.set_active_camera(1)
+            }
+            _ => {}
+        }
+        self.active_camera()
+            .process_keyboard(physical_key, logical_key, state)
     }
 }
